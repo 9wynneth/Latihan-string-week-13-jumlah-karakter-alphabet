b382099e346ff7b6d6416a78e9717de0b9b2f44f
9wynneth <gisviandhy@student.ciputra.ac.id>
2021-12-07 10:56:32 +0700

    Add project files.

---
+++ Latihan string week 13 jumlah karakter alphabet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Latihan_string_week_13_jumlah_karakter_alphabet
+{
+    class Program
+    {
+       static void StatistikHuruf()
+        {
+
+        }
+        static void StatistikKata(string InputanKata)
+        {
+            Console.WriteLine($"{InputanKata.ToUpper()}: KATA");
+        }
+        static void JumlahHuruf(string InputanKalimat)
+        {
+            Console.Write($"Jumlah huruf pada kalimat adalah: {InputanKalimat.Replace(" ", "").Length}");
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("INPUT KALIMAT:");
+            string InputanKalimat = Console.ReadLine();
+            string[] Potongan = InputanKalimat.Split(" ");
+            while (Potongan.Length<=3)
+            {
+                Console.Write("KATA PADA KALIMAT KURANG PANJANG \nINPUT KALIMAT:");
+                InputanKalimat = Console.ReadLine();
+                Potongan = InputanKalimat.Split(" ");
+            }
+            Console.WriteLine();
+            Console.Write("INPUT KATA: ");
+            string InputanKata = Console.ReadLine();
+            while (InputanKata.Length<=1)
+            {
+                Console.Write("HURUF PADA KATA KURANG PANJANG \nINPUT KATA:");
+                InputanKata = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.Write($"INPUT KALIMAT: {InputanKalimat.ToUpper()}\n");
+            Console.WriteLine($"INPUT KATA: {InputanKata.ToUpper()}\nOUTPUT:");
+            JumlahHuruf(InputanKalimat);
+        }
+    }
+}
